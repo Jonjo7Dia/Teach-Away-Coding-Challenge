6001ea098266675fea77d37c29a7837e4147cdde
Jonjo7Dia <jonosphgames@gmail.com>
2022-06-20 15:15:03 -0300

    redux implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import Gallery from './Gallery';
 function App() {
+  // Gallery();
   return (
     <div className="App">
      <h1>hello</h1>
+
     </div>
   );
 }
---
+++ src/store/filter-slice.js
@@ -0,0 +1,34 @@
+import { createSlice} from '@reduxjs/toolkit';
+
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState : {
+        section: 'hot',
+        sort: 'viral',
+        page: 0,
+        window: 'day',
+        showViral: true, 
+
+    },
+    reducers: {
+        setSection(state, action) {
+            state.section = action.payload;
+        },
+        setSort(state, action) {
+            state.sort = action.payload;
+        },
+        setPage(state, action){
+            state.page = action.payload;
+        },
+        setWindow(state, action){
+            state.window = action.payload;
+        },
+        setViral(state, action){
+            state.showViral = action.payload;
+        }
+    }
+});
+
+export const filterActions = filterSlice.actions;
+
+export default filterSlice;
